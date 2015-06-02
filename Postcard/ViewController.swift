d84dfb473782eb713e30657645a53fd793e160af
Jeyson Artiles <RA@Jeysons-Mac-Pro.local>
2015-06-02 14:40:06 -0400

    First Revision

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var sendersName: UILabel!
+    @IBOutlet weak var sendersMessage: UITextView!
+    
+    @IBOutlet weak var enterName: UITextField!
+    @IBOutlet weak var enterMessage: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("TEST TO CONSOLE!")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func postButton(sender: UIButton) {
+        
+        // CHECK 
+        if enterName.text == "" || enterMessage.text == "" {
+            sendersName.textColor = UIColor.whiteColor()
+            sendersName.backgroundColor = UIColor.redColor()
+            sendersName.text = "ENTER NAME & MESSAGE"
+            sendersName.hidden = false
+        } else {
+            // PRINT sendersName
+            sendersName.hidden = false
+            sendersMessage.hidden = false
+            
+            // CHANGE "POST" (postButton) to "SEND"
+            sender.setTitle("SENT", forState: .Normal)
+            sender.setTitleColor(UIColor.blackColor(), forState: .Normal)
+            sender.backgroundColor = UIColor.whiteColor()
+            
+            // REPLACE sendersName & sendersMessage placeholders with enterName & enterMessage; respectively
+            sendersName.text = enterName.text
+            sendersMessage.text = enterMessage.text
+        }
+        
+       
+        
+        // REMOVE keyboard on button press (postButton)
+        enterName.resignFirstResponder()
+        enterMessage.resignFirstResponder()
+    }
 
 }
 
